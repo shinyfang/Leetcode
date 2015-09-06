2f444a26b0c53c31fbc9b9deb053773f9e60ed8d
shinyfang <shinyfang1993@gmail.com>
2015-09-06 21:03:11 +0800

    right answer

--- src/Q2_Add_Two_Numbers.cpp
+++ src/Q2_Add_Two_Numbers.cpp
@@ -8,17 +8,53 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
+    	ListNode *out = new ListNode(0);
+    	ListNode *head = out;
+    	int flag = 0;
+    	while(l1 != NULL || l2 != NULL){
+    		if(l1 == NULL && l2 != NULL){
+    			out->next = new ListNode((l2->val+flag)%10);
+    			out = out->next;
+    			if (l2->val+flag >= 10)
+    				flag = 1;
+    			else
+    				flag = 0;
+    			l2 = l2->next;
+    		}
+    		else if (l1 != NULL && l2 == NULL){
+    			out->next = new ListNode((l1->val+flag)%10);
+    			out = out->next;
+    			if ((l1->val+flag) >= 10)
+    				flag = 1;
+    			else
+    				flag = 0;
+    			l1 = l1->next;
+    		}
+    		else{
+    			out->next = new ListNode((l1->val+l2->val+flag)%10);
+    			out = out->next;
+    			if ((l1->val+l2->val+flag) >= 10)
+    				flag = 1;
+    			else
+    				flag = 0;
+    			l1 = l1->next;
+    			l2 = l2->next;
+    		}
+    	}
+    	if(flag == 1)
+    		out->next = new ListNode(1);
+    	return head->next;
     }
 };
 int main(){
-	ListNode *l1 = new ListNode(2);
-	l1->next = new ListNode(4);
-	l1->next->next = new ListNode(3);
+	ListNode *l1 = new ListNode(5);
+	//l1->next = new ListNode(8);
+	//l1->next->next = new ListNode(3);
 	ListNode *l2 = new ListNode(5);
-	l2->next = new ListNode(6);
-	l2->next->next = new ListNode(4);
+	//l2->next = new ListNode(6);
+	//l2->next->next = new ListNode(4);
 	Solution s;
-	s.addTwoNumbers(l1,l2);
+	ListNode *out = s.addTwoNumbers(l1,l2);
+	cout<<out->val<<out->next->val;
 	return 0;
 }
